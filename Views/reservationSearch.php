eb1ed2954e694f31a050b7c930f50580a13f9285
Jordan Murphy <98192871+jmurphy007@users.noreply.github.com>
2025-03-03 22:41:00 -0500

    Final Update Before Report

--- Views/reservationSearch.php
+++ Views/reservationSearch.php
@@ -16,7 +16,6 @@
 		<title>Tropical Byte Hotel - Check Your Reservations</title>
 		<meta charset="utf-8" />
 		<meta name="author" content="Jordan Murphy & Farid Sawaqed" />
-		<meta name="description" content="{insert description here}"/>
 		<meta name="viewport" content="height= device-height, width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="../Components/Styles/styles.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
